8415f78b8e6cf7bbcba6121baed23de249b335f7
acmullin17 <acmullin17@gmail.com>
2026-01-05 10:19:50 -0600

    Add files via upload

---
+++ backend/smartAppointment/smartAppointment/src/test/java/com/revature/smartAppointment/Project1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.smartAppointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
