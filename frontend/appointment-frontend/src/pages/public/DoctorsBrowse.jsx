25d4e43eb061259d7282042992a9fdf7cc06866e
suy3sh <112516330+suy3sh@users.noreply.github.com>
2026-01-05 11:31:33 -0600

    created boilerplate public pages in front end

---
+++ frontend/appointment-frontend/src/pages/public/DoctorsBrowse.jsx
@@ -0,0 +1,12 @@
+function DoctorsBrowse() {
+    return (
+        <>
+            <div>
+                <h1>DoctorsBrowse Page</h1>
+                <p>src/pages/public/DoctorsBrowse.jsx</p>
+            </div>
+        </>
+    )
+}
+
+export default Login();
---
+++ frontend/appointment-frontend/src/pages/public/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <>
+            <dive>
+                <h1>Home Page</h1>
+                <p>src/pages/public/Home.jsx</p>
+            </dive>
+        </>
+    )
+}
+
+export default Home;
