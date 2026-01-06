5fc21ce01cd174f58a08471558b999d1b01ebd78
suy3sh <112516330+suy3sh@users.noreply.github.com>
2026-01-06 14:11:02 -0600

    added nav bar functionality

--- frontend/appointment-frontend/src/pages/public/GuestHome.jsx
+++ frontend/appointment-frontend/src/pages/public/GuestHome.jsx
@@ -1,32 +1,15 @@
 import { useState } from "react";
-import { FaHospital, FaUserDoctor } from "react-icons/fa6";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+
 
 function GuestHome() {
   const [selectedDate, setSelectedDate] = useState(null);
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-indigo-50 to-white">
-
-      {/* NAVBAR */}
-      <nav className="w-full bg-gradient-to-r from-indigo-500 to-purple-600 shadow">
-        <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center text-white">
-          
-          <div className="flex items-center gap-3 text-2xl font-bold">
-            <FaHospital />
-            Smart Appointment System
-          </div>
-
-          <div className="flex gap-6">
-            <button className="hover:underline">Home</button>
-            <button className="hover:underline">About</button>
-            <button className="hover:underline">Contact</button>
-          </div>
-
-        </div>
-      </nav>
-
+      
+    
       {/* HERO SECTION */}
       <section className="flex flex-col items-center text-center px-6 py-24">
         <h1 className="text-5xl font-extrabold text-gray-800 mb-6">
@@ -39,7 +22,7 @@ function GuestHome() {
 
         <div className="flex gap-6">
           <button className="bg-indigo-600 text-white px-8 py-3 rounded-xl text-lg font-semibold shadow-lg hover:bg-indigo-700 transition">
-            Sign Up
+            Login/Sign Up
           </button>
 
           <button className="border-2 border-indigo-600 text-indigo-600 px-8 py-3 rounded-xl text-lg font-semibold hover:bg-indigo-600 hover:text-white transition">
