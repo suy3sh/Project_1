16df7376e735739f2a5fcf155250d3d8b009d05f
acmullin17 <acmullin17@gmail.com>
2026-01-08 14:17:54 -0600

    Linked users to privilege

---
+++ backend/smartAppointment/src/main/java/com/revature/smartAppointment/Repository/PrivilegeRepository.java
@@ -0,0 +1,7 @@
+package com.revature.smartAppointment.Repository;
+
+import com.revature.smartAppointment.Model.Privilege;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrivilegeRepository extends JpaRepository<Privilege, Integer> {
+}
